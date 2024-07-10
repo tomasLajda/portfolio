b9bd7461e5577d82e92809f7a54f4072bda7b347
tomas.lajda <tomas.lajda0@gmail.com>
2024-07-10 14:02:54 +0200

    Added switch between dark theme and selected color pallete

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
+import { ModeToggle } from './components/mode-toggle';
+import { ThemeProvider } from './components/theme-provider';
+import { Button } from './components/ui/button';
+
 const App = () => {
   return (
-    <div>
-      <p></p>
-    </div>
+    <>
+      <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+        <ModeToggle />
+        <Button variant='default' />
+        <header>
+          <p>Logo</p>
+          <ul>
+            <li className=''>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contacts</li>
+          </ul>
+          <ul>
+            <li>Github</li>
+            <li>LinkedIn</li>
+          </ul>
+        </header>
+      </ThemeProvider>
+    </>
   );
 };
 
