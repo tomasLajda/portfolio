6969e73b807a3c27c2a2366cc7e881d836dd4539
Tomáš Lajda <69923181+tomasLajda@users.noreply.github.com>
2024-07-14 18:18:12 +0200

    making sure dirname is declared only once

--- netlify/functions/contact.js
+++ netlify/functions/contact.js
@@ -24,7 +24,8 @@ app.options('*', cors(corsOptions)); // Handle preflight requests
 app.use(express.static('public'));
 app.use(express.json());
 
-const __dirname = path.resolve()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename)
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'contact.html'));
