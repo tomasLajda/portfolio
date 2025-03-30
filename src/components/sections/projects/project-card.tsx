f32913d426722f8852b8888d31f3706ec7a155d4
tomasLajda <tomas.lajda0@gmail.com>
2025-03-30 17:03:11 +0300

    added hover effect to project card

--- src/components/sections/projects/project-card.tsx
+++ src/components/sections/projects/project-card.tsx
@@ -11,7 +11,7 @@ interface ProjectCardProps {
 const ProjectCard = ({ img, title, description, url }: ProjectCardProps) => {
   return (
     <CarouselItem className='lg:basis-1/2'>
-      <Card className='flex flex-col items-center p-5 bg-primary h-full'>
+      <Card className='flex flex-col items-center p-5 bg-primary h-full hover:bg-primary-hover'>
         <CardHeader className='min-h-28'>
           <CardTitle className='underline underline-offset-3 mb-4  hover:opacity-80'>
             <a href={url} target='_blank' className=''>
